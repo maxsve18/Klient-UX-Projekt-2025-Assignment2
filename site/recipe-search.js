3ca1a6d751553b6ccc0a6a9494dcc12de9023d4d
André Ljungsvik <andre.ljungsvik@gmail.com>
2025-05-02 16:56:39 +0200

    project complete

--- site/recipe-search.js
+++ site/recipe-search.js
@@ -3,13 +3,36 @@ document.querySelectorAll('.active-filters-clear, .filter-tag-btn, .allergen')
     .forEach(button => {
         button.addEventListener('click', () => {
             button.classList.toggle('selected');
+
+            const dropdownArrow = button.closest('.allergen-box')?.querySelector('.dropdown-arrow');
+            if (dropdownArrow) {
+                dropdownArrow.classList.add('hidden');
+            }
         });
     });
 
 //Box-toggle content visibility
-document.querySelectorAll('.box-toggle1, .box-toggle2').forEach(toggle => {
+document.querySelectorAll('.box-toggle1, .box-toggle2, .dropdown-arrow').forEach(toggle => {
     toggle.addEventListener('click', () => {
         const content = toggle.nextElementSibling;
         if (content) content.classList.toggle('hidden');
+
+        const dropdownArrow = toggle.querySelector('.dropdown-arrow');
+        if (dropdownArrow) {
+            dropdownArrow.classList.toggle('hidden');
+        }
     });
+});
+
+//Search result
+
+document.getElementById('search-input').addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        const query = event.target.value.trim();
+        if (query === "") return;
+
+        // Visa sökresultat, dölj filtervyn
+        document.getElementById('filter-options').hidden = true;
+        document.getElementById('recipeResults').hidden = false;
+    }
 });
